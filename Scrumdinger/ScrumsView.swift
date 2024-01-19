ca184c3e79aebe274ea62af847b4e9df668c8b65
Beka-l3 <b.talgat@innopolis.university>
2024-01-19 18:34:25 +0300

    Navigataion

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -15,11 +15,25 @@ struct ScrumsView: View {
     
     var body: some View {
         
-        List(scrums) { scrum in
+        NavigationStack {
             
-            CardView(scrum: scrum)
+            List(scrums) { scrum in
+                
+                NavigationLink(destination: Text(scrum.title)) {
+                
+                    CardView(scrum: scrum)
+                    
+                }
                 .listRowBackground(scrum.theme.mainColor)
-            
+                
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
         
     }
