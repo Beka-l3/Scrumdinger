ac225072af40efbd763784c98f83ae85f9f9ff4b
Beka-l3 <b.talgat@innopolis.university>
2024-01-19 20:47:10 +0300

    New Attendee

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct DetailEditView: View {
     
     @State private var scrum: DailyScrum = .emptyScrum
+    @State private var newAttendeeName: String = ""
     
     
     var body: some View {
@@ -17,11 +18,9 @@ struct DetailEditView: View {
         Form {
             
             Section {
-                
                 TextField("Title", text: $scrum.title)
                 
                 HStack {
-                    
                     Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
                         Text("Length")
                     }
@@ -29,10 +28,38 @@ struct DetailEditView: View {
                     Spacer()
                     
                     Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
+            
+            Section {
+                
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indexSet in
+                    scrum.attendees.remove(atOffsets: indexSet)
+                }
+                
+                HStack {
+                    
+                    TextField("New Attendee", text: $newAttendeeName)
+                    
+                    Button {
+                        withAnimation {
+                            scrum.attendees.append(DailyScrum.Attendee(name: newAttendeeName))
+                            newAttendeeName = ""
+                        }
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
                     
                 }
                 
+            } header: {
+                Text("Attendees")
             }
+            
             
         }
         
