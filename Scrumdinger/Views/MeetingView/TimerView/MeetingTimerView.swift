6cf65c58081dc0f4ee755003c9c05ceef925f688
Beka-l3 <b.talgat@innopolis.university>
2024-03-04 10:57:35 +0300

    Add SpeakerArc

--- Scrumdinger/Views/MeetingView/TimerView/MeetingTimerView.swift
+++ Scrumdinger/Views/MeetingView/TimerView/MeetingTimerView.swift
@@ -33,6 +33,19 @@ struct MeetingTimerView: View {
                 .accessibilityElement(children: .combine)
                 .foregroundStyle(theme.accentColor)
             }
+            .overlay {
+                ForEach(speakers) { speaker in
+                    if
+                        speaker.isCompleted,
+                        let index = speakers.firstIndex(where: { $0.id == speaker.id })
+                    {
+                        SpeakerArc(speakerIndex: index, totalSpeakers: speakers.count)
+                            .rotation(.init(degrees: -90))
+                            .stroke(theme.mainColor, lineWidth: 12)
+                    }
+                }
+            }
+            .padding(.horizontal)
     }
     
 }
